ae6c407f04f4ef80911ff646e920aeb32dd91d70
sohagislampk <sohagislampk@gmail.com>
2022-11-09 02:18:14 +0600

    Package summury card desinged

--- src/Pages/Home/Packages/Package.js
+++ src/Pages/Home/Packages/Package.js
@@ -1,8 +1,10 @@
 import { Button, Card } from 'flowbite-react';
 import React from 'react';
+import { BsCash, BsStopwatch } from 'react-icons/bs'
+
 
 const Package = ({ tour }) => {
-    const { name, picture, description } = tour;
+    const { name, picture, description, duration, price } = tour;
     return (
         <div>
             <div className="max-w-sm">
@@ -13,6 +15,16 @@ const Package = ({ tour }) => {
                     <p className="font-normal text-gray-700 dark:text-gray-400">
                         {description.slice(0, 100)} ...
                     </p>
+                    <div className='flex justify-between'>
+                        <div className='flex items-center'>
+                            <BsStopwatch className='text-xl text-sky-500'></BsStopwatch>
+                            <p className='mx-2'>{duration}</p>
+                        </div>
+                        <div className='flex items-center'>
+                            <BsCash className='text-xl text-sky-500'></BsCash>
+                            <p className='mx-2'> ${price}</p>
+                        </div>
+                    </div>
                     <div className='mx-auto'>
                         <Button gradientMonochrome="cyan">
                             View Details
--- src/Pages/Home/Packages/Packages.js
+++ src/Pages/Home/Packages/Packages.js
@@ -14,7 +14,7 @@ const Packages = () => {
     return (
         <div>
             <h1 className='w-1/3 mx-auto rounded-lg py-2 my-8 bg-sky-400 text-white text-3xl font-bold'>Our Tour Packages</h1>
-            <div className='grid grid-cols-3 gap-6'>
+            <div className='grid grid-cols-3 gap-6 mx-8'>
                 {
                     packages.map(tour => <Package
                         key={tour._id}
