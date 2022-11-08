6958a5f4181608d284c92e0ad40580676b0110f2
sohagislampk <sohagislampk@gmail.com>
2022-11-08 13:50:25 +0600

    Header banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,44 @@
+
+import { Button, Carousel } from 'flowbite-react';
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="h-56 lg:h-96">
+                <Carousel>
+                    <div className="flex flex-col h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white banner-1 banner-overlay">
+                        <h3 className='text-lg text-white z-50' >Experiences The Largest See Beach</h3>
+                        <h1 className='text-white text-5xl z-50 my-6' >Bangladesh Is Open For Travel</h1>
+                        <div className='z-50 mt-6'>
+                            <Button gradientDuoTone="greenToBlue">
+                                Explor The Packages
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="flex flex-col h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white banner-2 banner-overlay">
+                        <h3 className='text-lg text-white z-50' >See World Like Never before</h3>
+                        <h1 className='text-white text-5xl z-50 my-6' >Travel with Travel On The Go</h1>
+                        <div className='z-50 mt-6'>
+                            <Button gradientDuoTone="greenToBlue">
+                                Explor The Packages
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="flex flex-col h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white banner-3 banner-overlay">
+                        <h3 className='text-lg text-white z-50' >Travel is your therapy</h3>
+                        <h1 className='text-white text-5xl z-50 my-6' >Dare to live the life you've always wanted</h1>
+                        <div className='z-50 mt-6'>
+                            <Button gradientDuoTone="greenToBlue">
+                                Explor The Packages
+                            </Button>
+                        </div>
+                    </div>
+                </Carousel>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main";
 import Blog from "../../Pages/Blog/Blog";
-import Home from "../../Pages/Home/Home";
+import Home from "../../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
 
