d39c3b6192fae9ae5bb53fdd6b40ec25f57cd517
sohagislampk <sohagislampk@gmail.com>
2022-11-09 22:07:40 +0600

    Delete reviews added

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,24 +1,55 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 import MyreviewCard from './MyreviewCard';
 
 const MyReviews = () => {
+    const { user } = useContext(AuthContext)
     const [allReviews, setAllReviews] = useState([])
 
     useEffect(() => {
-        fetch(`http://localhost:5000/reviews`)
+        fetch(`http://localhost:5000/reviews?email=${user?.email}`)
             .then(res => res.json())
             .then(data => setAllReviews(data))
             .catch(error => console.error(error));
     }, [allReviews])
 
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure, you want to delete this review');
+        if (proceed) {
+            fetch(`http://localhost:5000/reviews/${id}`, {
+                method: 'DELETE',
+                headers: {
+
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('Review deleted successfully');
+                        const remaining = allReviews.filter(review => review._id !== id);
+                        setAllReviews(remaining);
+                    }
+                })
+        }
+    }
+
+
+
+
+
     return (
         <div>
+            <h1 className='w-1/3 mx-auto rounded-lg py-2 my-8 bg-sky-400 text-white text-3xl font-bold'>All Reviews</h1>
             {
                 allReviews.map(reviews => <MyreviewCard
                     key={reviews._id}
                     reviews={reviews}
+                    handleDelete={handleDelete}
                 ></MyreviewCard>)
+
+                // <p className='text-xl text-center my-40'>No reviews were added</p>
+
             }
         </div>
     );
--- src/Pages/MyReviews/MyreviewCard.js
+++ src/Pages/MyReviews/MyreviewCard.js
@@ -1,25 +1,37 @@
-import { Avatar, Card } from 'flowbite-react';
+import { Avatar, Card, Tooltip } from 'flowbite-react';
 import React from 'react';
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
+const MyreviewCard = ({ reviews, handleDelete }) => {
+    const { writerName, writerPhoto, review, _id } = reviews;
 
-const MyreviewCard = ({ reviews }) => {
-    const { writerName, writerPhoto, review } = reviews;
     return (
         <div>
-            <div className='text-left'>
+            <div className='flex items-center justify-around'>
+                <div className='text-left m-4 w-2/3'>
+                    <Card>
+                        <div className="flex flex-wrap gap-2 items-center">
+                            <Avatar
+                                img={writerPhoto}
+                                rounded={true}
+                                bordered={true}
+                            />
+                            <p className='text-xl font-semibold'>{writerName}</p>
+                        </div>
+                        <p >{review}</p>
+                    </Card>
+                </div>
+                <div className='flex items-center'>
+                    <Tooltip content="Edit Review">
+                        <Card className='mr-4'>  <AiFillEdit className='text-2xl text-cyan-500'></AiFillEdit></Card>
+                    </Tooltip>
+                    <Tooltip content="Delete Review">
+                        <Card onClick={() => handleDelete(_id)} className='mr-4'><AiFillDelete className='text-2xl text-red-500'></AiFillDelete></Card>
+                    </Tooltip>
 
-                <Card>
-                    <div className="flex flex-wrap gap-2 items-center">
-                        <Avatar
-                            img={writerPhoto}
-                            rounded={true}
-                            bordered={true}
-                        />
-                        <p className='text-xl font-semibold'>{writerName}</p>
-                    </div>
-                    <p >{review}</p>
-                </Card>
+                </div>
             </div>
         </div>
+
     );
 };
 
